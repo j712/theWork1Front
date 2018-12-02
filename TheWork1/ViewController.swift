c4153489d73a048427fccbca4353a7d92792aae5
umeda <the_curry_is_drink@yahoo.co.jp>
2018-12-03 01:04:19 +0900

    Initial Commit

---
+++ TheWork1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheWork1
+//
+//  Created by umeda yodai on 2018/12/03.
+//  Copyright © 2018 umeda yodai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TheWork1Tests/TheWork1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  TheWork1Tests.swift
+//  TheWork1Tests
+//
+//  Created by umeda yodai on 2018/12/03.
+//  Copyright © 2018 umeda yodai. All rights reserved.
+//
+
+import XCTest
+@testable import TheWork1
+
+class TheWork1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
